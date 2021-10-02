a323d6dd10e73cafffdcd19a7c4399a93ca2efaa
lawrencejews <lawrencejews1@gmail.com>
2021-10-02 14:08:00 +0900

    jamstack-gatsby

---
+++ src/components/route-base.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RouteBase = () => <h1>All your base belongs here</h1>
+
+export default RouteBase;
---
+++ src/components/route-secret.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RouteSecret = () => <h1>The secret are here</h1>
+
+export default RouteSecret;
